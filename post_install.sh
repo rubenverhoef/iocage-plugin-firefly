63324e48354dd0b73f1b88895b79e1732def1251
Ruben Verhoef <ruben_verhoef@hotmail.com>
2019-08-10 12:10:10 +0200

    Move installation to sepearte bash script

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,1 @@
 #!/bin/sh
-
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-cd /usr/local/www && composer create-project grumpydictator/firefly-iii --no-dev --prefer-dist firefly-iii 4.8.0
